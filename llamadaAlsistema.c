057362621ee8f135b66d86cf55a39ba1c5966497
EstebanMV96 <moyaco41@gmail.com>
2016-09-04 12:57:18 -0500

    Lls

---
+++ llamadaAlsistema.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+
+ int main()
+
+ {         // Abre main
+  printf("\nEsta es una llamada al sistema en C:\n");
+
+  // El comando ls despliega los archivos contenidos
+  // en el directorio actual
+
+  system("ls");
+
+  return 0;
+ }         // Cierra main
